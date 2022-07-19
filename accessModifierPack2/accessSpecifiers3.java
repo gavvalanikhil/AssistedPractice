40409fd14f017513946a52296181df404f3fedb4
nikhil <nikhilgavvala6050@gmail.com>
2022-07-19 22:24:39 +0530

    protected and public explained in 2nd package

---
+++ accessModifierPack2/accessSpecifiers3.java
@@ -0,0 +1,11 @@
+package accessModifierPack2;
+
+import accessModifierPack1.*;
+public class accessSpecifiers3 extends proaccessspecifiers {
+	public static void main(String[] args) {
+		accessSpecifiers3 obj = new accessSpecifiers3 ();   
+	       obj.display();  
+	}
+
+
+}
---
+++ accessModifierPack2/accessSpecifiers4.java
@@ -0,0 +1,13 @@
+package accessModifierPack2;
+import accessModifierPack1.*;
+
+public class accessSpecifiers4 {
+public static void main(String[] args) {
+		
+		pubaccessspecifiers obj = new pubaccessspecifiers(); 
+        obj.display();  
+		
+	}
+
+
+}
