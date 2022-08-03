2069848264baa3ccc34ed568d3d515627a5dba7c
nikhil <nikhilgavvala6050@gmail.com>
2022-08-03 19:17:11 +0530

    phase 2 pgms committed successfully

---
+++ phase 2 practice projects/FilterDemo/src/main/java/com/LoginFilter.java
@@ -0,0 +1,85 @@
+package com;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginFilter
+ */
+public class LoginFilter implements Filter {
+
+    /**
+* Default constructor.
+*/
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see Filter#destroy()
+         */
+        public void destroy() {
+                // TODO Auto-generated method stub
+        }
+
+        /**
+         * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+         */
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                // TODO Auto-generated method stub
+                // place your code here
+                
+                String userId = request.getParameter("userid");
+
+                if( userId != null){
+                        chain.doFilter(request, response);
+            }
+                 
+                
+        }
+
+        /**
+         * @see Filter#init(FilterConfig)
+         */
+        public void init(FilterConfig fConfig) throws ServletException {
+                // TODO Auto-generated method stub
+        }
+
+}
+
+//public class LoginFilter extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//       
+//    /**
+//     * @see HttpServlet#HttpServlet()
+//     */
+//    public LoginFilter() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+//
+//	/**
+//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+//
+//}
